fba2a78f58004d46c9832ca4b104d4383d6e1bf3
Carlos Fuentes <metcdoder95@MacBook-Pro-de-Carlos.local>
2019-06-22 14:41:30 +0200

    Implementation added

--- challenge.ts
+++ challenge.ts
@@ -1,31 +1,60 @@
+import ShipmentQueue, { OperationResult } from "./shipmentQueue";
+
 async function sleep(ms: number) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => resolve(), ms)
-    })
+  return new Promise((resolve, reject) => {
+    setTimeout(() => resolve(), ms);
+  });
 }
 
 async function randomDelay() {
-    const randomTime = Math.round(Math.random() * 1000)
-    return sleep(randomTime)
+  const randomTime = Math.round(Math.random() * 1000);
+  return sleep(randomTime);
 }
 
 class ShipmentSearchIndex {
-    async updateShipment(id: string, shipmentData: any) {
-        const startTime = new Date()
-        await randomDelay()
-        const endTime = new Date()
-        console.log(`update ${id}@${
-            startTime.toISOString()
-            } finished@${
-            endTime.toISOString()
-            }`
-        )
+  async updateShipment(id: string, shipmentData: any) {
+    const startTime = new Date();
+    await randomDelay();
+    const endTime = new Date();
+    console.log(
+      `update ${id}@${startTime.toISOString()} finished@${endTime.toISOString()}`
+    );
 
-        return { startTime, endTime }
-    }
+    return { startTime, endTime };
+  }
 }
 
 // Implementation needed
 interface ShipmentUpdateListenerInterface {
-    receiveUpdate(id: string, shipmentData: any)
+  receiveUpdate(id: string, shipmentData: any);
 }
+
+// Implementation
+class ShipmentUpdateListener implements ShipmentUpdateListenerInterface {
+  private shipmentSearch: ShipmentSearchIndex;
+  private shipmentQueue: ShipmentQueue;
+
+  constructor() {
+    this.shipmentSearch = new ShipmentSearchIndex();
+    this.shipmentQueue = new ShipmentQueue();
+  }
+
+  async receiveUpdate(id: string, shipmentData: any) {
+    const promise: Promise<
+      OperationResult
+    > = this.shipmentSearch.updateShipment(id, shipmentData);
+    await this.shipmentQueue.addToQueue(id, promise);
+  }
+}
+
+const shipmentUpdateListener = new ShipmentUpdateListener();
+
+shipmentUpdateListener
+  .receiveUpdate("1", { data: { more: "data" } })
+  .then(() => {
+    return shipmentUpdateListener.receiveUpdate("1", {
+      seconData: { more: "data" }
+    });
+  })
+  .then(() => console.log("Final promise"))
+  .catch(console.error);
